10c93053459c2c5e5de29c208bc6bb086c04a4c1
Hannah Hoff <hannahhoff@Hannahs-MacBook-Pro.local>
2019-04-09 13:40:20 -0600

    first commit with storyboard

---
+++ Continuum25/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Continuum25
+//
+//  Created by Hannah Hoff on 4/9/19.
+//  Copyright © 2019 Hannah Hoff. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+}
+
+
